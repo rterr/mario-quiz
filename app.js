d99b505a5536c5e8044b4744da498c8c7c60333f
R. Terrado <r.terrado@gmail.com>
2016-07-24 20:07:47 -0700

    Complete

---
+++ app.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+	// initial variables and array shuffle
+	var questionCounter = 0;
+	var scoreCounter = 0;
+	shuffle(questionList);
+
+	//start next question
+	 $('span.next-question').click(function() {
+	 	//toggle off what was either the start-page or result-page
+		$(this).parent('div').toggle();
+
+		//check if questionCounter > 4 (end of game, toggles final result screen)
+		if (questionCounter > 4) {
+			$('div#result-final').toggle();
+		}
+		//else continue with displaying next question
+		//display current question # to scorebar, display question + options
+		else {
+			$('span#question-counter-number').text(questionCounter + 1);
+	   		$('#question-page h2').text(questionList[questionCounter].question);
+	   		//iterate through options 0 - 3 and display text to respective spans
+	   		for (var i = 0; i < 5; i++){
+	   			var optionId = "span#" + i;
+	   			$(optionId).text(questionList[questionCounter].options[i]);
+	   		}
+		}
+
+	 });
+
+	//user selects an answer
+	$('span.answer-option').click(function(){
+		//toggle on result-page (covers question-page)
+		$('div#result-page').toggle();
+		//display correct answer and trivia for prior question
+		$('span#correct-answer').text(questionList[questionCounter].options[questionList[questionCounter].correctAnswer])
+		$('p#trivia-text').text(questionList[questionCounter].trivia)
+
+		//if correct, +1 to score and display positive response
+		if (event.target.id == parseInt(questionList[questionCounter].correctAnswer)) {
+			scoreCounter++;
+			$('h2#right-or-wrong').text('You got it!');
+		}
+		//else, score isn't updated and display negative response
+		else { 
+			$('h2#right-or-wrong').text('Incorrect...');
+		}
+		//display current score to the scorebar
+		$('span.score-counter-number').text(scoreCounter);
+		//move on to next question
+		questionCounter++;
+	 });
+
+	//user selects new game
+	$('span.new-game').click(function(){
+		//reset the questionCounter and scoreCounter, run shuffle(questionList);
+		questionCounter = 0;
+		scoreCounter = 0;
+		shuffle(questionList);
+		$('span.score-counter-number').text(scoreCounter);
+		//toggle off result-final and toggle on start-page div
+		$('div#result-final').toggle();
+		$('div#start-page').toggle();
+	});
+
+//end
+});
+
+//Fisher-Yates shuffle
+function shuffle(array) {
+	var m = array.length, t, i;
+	// While there remain elements to shuffle
+	while (m) {
+		// Pick a remaining element
+		i = Math.floor(Math.random() * m--);
+		// And swap it with the current element
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+		}
+  	return array;
+	}
---
+++ questions.js
@@ -0,0 +1,81 @@
+var questionOne = {
+		questionNumber: 1,
+		question: 'What was the first game that Mario starred in?',
+		correctAnswer: 1,
+  		options: ['Mario Bros.', 'Donkey Kong', 'Balloon Fight', 'Marble Madness'],
+  		trivia: 'Donkey Kong debuted in arcades in 1981, and at that time Mario was named \"Jumpman.\"',
+};
+
+var questionTwo = {
+		questionNumber: 2,
+		question: 'Who is the main antagonist in the Mario series?',
+		correctAnswer: 3,
+		options: ['Wario', 'Wart', 'Yoshi', 'Bowser'],
+		trivia: 'The fearsome, fire-breathing Bowser is named Koopa in Japan.',
+};
+
+var questionThree = {
+		questionNumber: 3,
+		question: 'What are the fungus-like enemies of the Mario games called?',
+		correctAnswer: 2,
+		options: ['Toad', 'Funguy', 'Goomba', 'Portabello'],
+		trivia: 'The Goomba\'s Japanese name, Kuribo, means \"chestnut guy.\"',
+};
+
+var questionFour = {
+		questionNumber: 4,
+		question: 'What is Mario\'s occupation?',
+		correctAnswer: 0,
+		options: ['Plumber', 'Archaeologist', 'Mechanic', 'Pizza Deliveryman'],
+		trivia: 'In the Super Mario Bros Super Show cartoon, Mario is a plumber from Brooklyn who ends up in the Mushroom Kingdom by way of a magic drain pipe.',
+};
+
+var questionFive = {
+		questionNumber: 5,
+		question: 'In the Mario Kart series, what is the item that shrinks other racers?',
+		correctAnswer: 2,
+		options: ['Banana Peel', 'Red Shell', 'Lightning Bolt', 'Mushroom'],
+		trivia: 'Super Mario Kart debuted on Super Nintendo in 1992, and essentially created the combat-oriented kart-racing video game genre.',
+};
+
+var questionSix = {
+		questionNumber: 6,
+		question: 'What objects does Mario have to collect in Super Mario 64 to advance the game?',
+		correctAnswer: 1,
+		options: [ 'Coins', 'Stars', 'Mushrooms', 'Eggs'],
+		trivia: 'Super Mario 64 (Nintendo 64, 1996) was the first 3D Mario game and set a standard for 3D adventure platform games.',
+};
+
+var questionSeven = {
+		questionNumber: 7,
+		question: 'Mario\'s little brother, Luigi, starred in Luigi\'s Mansion. What console did it debut on in 2001?',
+		correctAnswer: 3,
+		options: ['Game Boy', 'Virtual Boy', 'Wii', 'GameCube'],
+		trivia: 'Luigi first appeared in the 1983 arcade hit Mario Bros., where the titular brothers must exterminate monsters coming from the sewers.',
+};
+
+var questionEight = {
+		questionNumber: 8,
+		question: 'What is Bowser\'s group of mischievous children called?',
+		correctAnswer: 0,
+		options: ['Koopalings', 'Koopa Klub', 'Turtle Trouble', 'Dinosores'],
+		trivia: 'All seven Koopalings are named after famous musicians, and they first appeared in Super Mario Bros. 3 (Nintendo Entertainment System, 1988).',
+};
+
+var questionNine = {
+		questionNumber: 9,
+		question: 'In Super Mario Bros. 3, what animal does Mario turn into in order to fly?',
+		correctAnswer: 2,
+		options: ['Squirrel', 'Parrot', 'Raccoon', 'Hedgehog'],
+		trivia: 'Other animal-themed powers in Super Mario Bros 3 include the Frog suit, which makes swimming in water easier, and the Tanooki suit, which allows the user to transform into an invulnerable statue.',
+};
+
+var questionTen = {
+		questionNumber: 10,
+		question: 'What was Princess Peach\'s official name in North America prior to Super Mario 64?',
+		correctAnswer: 1,
+		options: ['Princess Zelda', 'Princess Toadstool', 'Princess Pineapple', 'Princess Kate'],
+		trivia: 'The starring princess in Super Mario Land (Game Boy, 1989) was named Daisy.',
+};
+
+var questionList = [questionOne,questionTwo,questionThree,questionFour,questionFive,questionSix,questionSeven,questionEight,questionNine,questionTen];
